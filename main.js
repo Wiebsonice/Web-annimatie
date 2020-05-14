07c9260c23160130f7e6a7ca52e767f6f5ad0e3c
Wiebe Kremer <wiebe.kremer96@gmail.com>
2020-05-14 11:27:07 +0200

    test

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("hey")
+
+// classname = document.getElementsByClassName("hello");
+//
+// for (i = 0; i < classname.length; i++) {
+//     classname[i].addEventListener('click', function() {
+//         this.className = 'done';
+//     }
+// }
