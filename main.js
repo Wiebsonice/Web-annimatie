40883afb2eda42656e91a42329bc5ee5c49d2aae
Wiebe Kremer <wiebe.kremer96@gmail.com>
2020-05-18 14:28:58 +0200

    added new eye track

--- main.js
+++ main.js
@@ -31,16 +31,54 @@ function getScrollPercent() {
 }
 
 var pupil = document.querySelector(".eye");
-
+//
 document.addEventListener("mousemove", (e) => {
-	let x = e.clientX * 100 / window.innerWidth - 38;
-	let y = e.clientY * 100 / window.innerHeight - 31;
+	let x = e.clientX * 30 / window.innerWidth - 11.4;
+	let y = e.clientY * 30 / window.innerHeight - 11.3;
 
-    console.log(y)
+    if (y >= 10 && x >= 11.4) {
+        pupil.style.transform = 'translate(' + 11.4 + 'px ,' + 10 + 'px)';
+    } else if (y >= 10) {
+        pupil.style.transform = 'translate(' + x + 'px ,' + 10 + 'px)';
+    } else if (x >= 11.4) {
+        pupil.style.transform = 'translate(' + 11.4 + 'px ,' + y + 'px)';
+    } else {
+        pupil.style.transform = 'translate(' + x + 'px ,' + y + 'px)';
+    }
+
+    console.log("y:", y, "x", x)
     //
 	// pupil.style.transform = "translate(" + x + "," + y + ")" ;
-    pupil.style.transform = 'translateX(" + x + "px)';
-	pupil.style.y= y;
-    pupil.style.x= x;
+
+	// pupil.style.y= y;
+    // pupil.style.x= x;
     // pupil.style.opacity = getScrollPercent();
 })
+
+
+// let eyeBall = document.querySelector(".eyeball");
+// let pupil = document.querySelector(".pupil");
+// let eyeArea = eyeBall.getBoundingClientRect();
+// let pupilArea = pupil.getBoundingClientRect();
+// let R = eyeArea.width/2;
+// let r = pupilArea.width/2;
+// let centerX = eyeArea.left + R;
+// let centerY = eyeArea.top + R;
+//
+// console.log(eyeArea)
+// console.log(pupilArea)
+// console.log(R)
+// console.log(r)
+// console.log(centerX)
+// console.log(centerY)
+//
+// document.addEventListener("mousemove", (e)=>{
+//   let x = e.clientX - centerX,
+//       y = e.clientY - centerY,
+//       theta = Math.atan2(y,x),
+//       angle = theta*180/Math.PI + 360;
+//
+//
+//   pupil.style.transform = `translateX(${R - r +"px"}) rotate(${angle + "deg"})`;
+//   pupil.style.transformOrigin = `${r +"px"} center`;
+// });
